87d0fcd68a21a3317ed41b0d22af8812513795cd
performautodev <ala.garbaa.pro@gmail.com>
2023-06-09 14:01:10 +0100

    Fixed type error in the specified file by ensuring that the 'photo' prop passed to the 'Photo' component is of type 'PhotoType' and not undefined.
    Fixed type error in ./app/@modal/(.)photos/[id]/page.tsx:19. Ensure that the 'photo' prop passed to the 'Photo' component is of type 'PhotoType' and not undefined

--- app/@modal/(.)photos/[id]/page.tsx
+++ app/@modal/(.)photos/[id]/page.tsx
@@ -16,7 +16,8 @@ export default function PhotoModal({
 
   return (
     <Modal>
-      <Photo photo={photo} />
+      {photo && <Photo photo={photo} />}
+      {!photo && "Photo not found"}
     </Modal>
   );
 }
--- app/photos/[id]/page.tsx
+++ app/photos/[id]/page.tsx
@@ -14,7 +14,8 @@ export default function PhotoPage({ params: { id } }: PhotoPageProps) {
   return (
     <div className="container mx-auto my-10">
       <div className="w-1/2 mx-auto border border-gray-700">
-        <Photo photo={photo} />
+        {photo && <Photo photo={photo} />}
+        {!photo && "Photo not found"}
       </div>
     </div>
   );
